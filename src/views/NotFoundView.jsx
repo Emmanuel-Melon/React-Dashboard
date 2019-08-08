ae69c92261edd27fe0c74e2f6947269724d18718
Eman <emmanuelgatwech@gmail.com>
2019-08-09 00:19:31 +0300

    Added not found page

---
+++ src/views/NotFoundView.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+/**
+ * grid
+ */
+import GridContainer from "../components/Grid/GridContianer";
+import GridItem from "../components/Grid/GridItem";
+
+/**
+ * material-ui
+ */
+import Typgoraphy from "@material-ui/core/Typography";
+
+/**
+ * core components
+ */
+
+/**
+ * styles
+ */
+import {makeStyles} from '@material-ui/core/styles';
+const useStyles = makeStyles(theme => (
+    {
+        Content: {
+            marginBottom: 16
+        },
+        Wrapper: {
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100%"
+        }
+    }
+));
+
+
+/**
+ * @return {*}
+ * @constructor
+ */
+const NotFoundView = () => {
+    const classes = useStyles();
+    return (
+        <div className={classes.Wrapper}>
+                    <div className={classes.Content}>
+                        <Typgoraphy variant={"h1"}>
+                            Page Not Found :(
+                        </Typgoraphy>
+                    </div>
+        </div>
+    )
+};
+
+export default NotFoundView;
