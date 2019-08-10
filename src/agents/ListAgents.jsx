fe95ae4ee36e1c264c69c4dd3532520e2cb67126
Eman <emmanuelgatwech@gmail.com>
2019-08-10 23:02:31 +0300

    Created basic agent fetching functionality

--- src/agents/ListAgents.jsx
+++ src/agents/ListAgents.jsx
@@ -1,78 +1,58 @@
-// <User key={id} user={user} />
-// look at using component thumbnail as links
-// also link vs nav link
 import React from "react";
-import { withRouter, NavLink, Redirect } from "react-router-dom";
+import { withRouter, Redirect } from "react-router-dom";
 
 import Agent from "./AgentOverview";
 import ManageAgents from "./ManageAgents";
 
 import fetchData from '../services/api';
 import ErrorComponent from "../components/Error/ErrorComponent";
-
-const users = [
-    {
-        avatar: "https://bo.training/wp-content/uploads/2017/08/employee-training-avatar-full-size-v1.png",
-        id: 1,
-        name: "Emmanuel Gatwech",
-        title: "Garbage Collector"
-    },
-    {
-        avatar: "http://www.learnex.in/wp-content/uploads/2015/12/flat-faces-icons-circle-6.png",
-        id: 2,
-        name: "Kengo Louis",
-        title: "Garbage Collector"
-    },
-    {
-        avatar: "https://www.pngarts.com/files/3/Employee-Avatar-PNG-Free-Download.png",
-        id: 3,
-        name: "Atem Aguer",
-        title: "Garbage Collector"
-    },
-    {
-        avatar: "https://www.pngarts.com/files/3/Employee-Avatar-PNG-Free-Download.png",
-        id: 4,
-        name: "Vibm Mayanja",
-        title: "Garbage Collector"
-    }
-];
+import _ from "lodash";
+import Spinner from "../components/Spinners/Spinner";
 
 
 class ListUsers extends React.Component {
     state = {
         selectedUser: null,
         isFetching: false,
-        data: [],
+        agents: [],
         error: false,
         notFound: false,
-    }
+    };
 
     componentDidMount() {
         this.handleFetchingAgents();
     }
 
     handleFetchingAgents = async () => {
+        this.setState({ isFetching: true });
         const { error, notFound } = this.state;
-        const result = await fetchData('provider/GarbageCollectors/binit/agents');
+        const result = await fetchData('provider/GarbageCollectors/homeklin/agents');
         const { errorMessage, data } = result;
+        let { agents } = data;
+        agents = _.values(agents);
+        console.table(agents);
         this.setState({
+            agents,
             error: errorMessage && !error,
-            notFound: (data && data.agents.undefined) && !notFound
-        })
-    }
+            isFetching: false,
+            notFound: (data && agents.undefined) && !notFound
+        });
+    };
 
     handleClick (id) {
-        console.log(id);
         this.setState({ selectedUser: id });
     }
     render () {
         const { error, notFound } = this.state;
         if(this.state.selectedUser) {
-            console.log("yessir!");
             return <Redirect to={`/agents/${this.state.selectedUser}`} />
         }
-        const { match } = this.props;
-        console.log(match);
+
+        if(this.state.isFetching === true) {
+            return (
+                <Spinner/>
+            );
+        }
         return (
             <div>
                 <div>
@@ -85,7 +65,7 @@ class ListUsers extends React.Component {
                     !(notFound || error) && (
                         <div>
                             {
-                                users.map((user) => {
+                                this.state.agents.map((user) => {
                                     return (
                                         <div
                                             key={`${user.id}`}
