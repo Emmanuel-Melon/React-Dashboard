06d658653c4bee3a2feb667d2216b425f276fbee
Eman <emmanuelgatwech@gmail.com>
2019-08-11 10:40:39 +0300

    Created logout component

--- src/components/Auth/Logout.jsx
+++ src/components/Auth/Logout.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 
+/**
+ * material-ui/core
+ */
+import { Button }from "@material-ui/core";
+
+/**
+ * HOCs
+ */
+import withFirebase from "../../HOCs/withFirebase";
 import { compose } from "recompose";
 import { withRouter } from "react-router-dom";
-import { Button }from "@material-ui/core";
 
 /**
  * styles
@@ -20,8 +28,6 @@ const logoutStyles = theme => ({
 
 });
 
-
-
 class LogoutButton extends Component {
 
     constructor (props) {
@@ -29,12 +35,14 @@ class LogoutButton extends Component {
         this.state = {
             clicked: false
         };
-        this.handleClick = this.handleClick.bind(this);
     }
 
-    handleClick () {
-        // this.props.firebase.doSignOut();
-        this.props.history.push("/landing");
+    async handleClick () {
+        try {
+            const user = await this.props.firebase.doSignOut();
+        } catch (e) {
+
+        }
     }
 
     render () {
@@ -50,6 +58,7 @@ class LogoutButton extends Component {
 }
 
 export default compose(
+    withFirebase,
     withRouter,
     withStyles(logoutStyles)
 )(LogoutButton);
