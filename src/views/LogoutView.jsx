77bdcda7b7350749b9f10c358ac2c6231a803733
Eman <emmanuelgatwech@gmail.com>
2019-08-11 13:26:37 +0300

    Modified logout view

--- src/views/LogoutView.jsx
+++ src/views/LogoutView.jsx
@@ -5,9 +5,15 @@ import {
 } from "@material-ui/core";
 
 /**
+ * context
+ */
+import { AuthConsumer } from "../services/auth";
+
+/**
  * core components
  */
 import Logout from "../components/Auth/Logout";
+import Spinner from "../components/Spinners/Spinner";
 
 /**
  * styles
@@ -31,22 +37,35 @@ const useStyles = makeStyles(theme => (
 /**
  * @return {*}
  * @constructor
+ * TODO: display error message when spinner times out
+ * * Observation page reloads when spinner times out.
  */
 const LogoutView = () => {
     const classes = useStyles();
     return (
-        <div className={classes.Wrapper}>
-            <div className={classes.Content}>
-                <div>
-                    <Typography variant={"h5"}>
-                        You're logged in as Emmanuel Gatwech
-                    </Typography>
-                    <div>
-                        <Logout />
+        <AuthConsumer>
+            { authUser => {
+                console.log(authUser);
+                return (
+                    <div className={classes.Wrapper}>
+                        <div className={classes.Content}>
+                            { (authUser !== null ) ? (
+                                <div>
+                                    <Typography variant={"h5"}>
+                                        You're logged in as {authUser.email}.
+                                    </Typography>
+                                    <div>
+                                        <Logout />
+                                    </div>
+                                </div>
+                            ) : (
+                                <Spinner/>
+                            )}
+                        </div>
                     </div>
-                </div>
-            </div>
-        </div>
+                )
+            }}
+        </AuthConsumer>
     )
 };
 
