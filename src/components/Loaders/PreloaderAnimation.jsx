883f5cf3bd9b7bbab0e053cf0a56b4185ec505cf
Eman <emmanuelgatwech@gmail.com>
2019-08-14 14:48:16 +0300

    initial commit for preloader animation component

---
+++ src/components/Loaders/PreloaderAnimation.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function PreloaderAnimation() {
+    return (
+        <div className="Preloader">
+            <h3>Loading</h3>
+        </div>
+    )
+
+}
